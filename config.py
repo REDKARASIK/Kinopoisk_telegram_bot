f648d56cb6ad28a2d66d446378275bf96751c51a
maver-ya <maver2005@ya.ru>
2023-04-06 15:47:47 +1000

    added config.py + head of main code

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = ''
+KEY_APY = ''
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('HEllO')
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
