25a9f825d7798de427033e608dc5d4b21ef3bd5e
REDKARASIK <redkarasik@yandex.ru>
2023-04-08 15:16:44 +1000

    make functions.py and make main in main.py

---
+++ functions.py
@@ -0,0 +1,6 @@
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+
+async def start(update, context):
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from functions import *
 from telegram.ext import Application, MessageHandler, filters
 from config import BOT_TOKEN
 
@@ -7,3 +8,12 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
