c5f80f92c63c463fa3a155bc326051bd8d686bb1
REDKARASIK <redkarasik@yandex.ru>
2023-04-11 15:38:43 +1000

    add search_film function and correct text

--- functions.py
+++ functions.py
@@ -12,7 +12,8 @@ async def start(update, context):
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("HELLO", reply_markup=reply_markup)
+    await update.message.reply_text(
+        "Добро пожаловать в стартовое меню бота.\nЗдесь вы можете найти нужную вам функцию.", reply_markup=reply_markup)
 
 
 async def button(update, context):
@@ -22,3 +23,11 @@ async def button(update, context):
     await query.edit_message_text(f"Вы нажали {query.data}")
 
 
+async def search_film(update, context):
+    keyboard = [[InlineKeyboardButton('Поиск фильмов по названию', callback_data='search_by_name'),
+                 InlineKeyboardButton('Поиск фильмов по актёру', callback_data='search_by_actor')],
+                [InlineKeyboardButton('Поиск по фильмов по режиссёру', callback_data='search_by_director'),
+                 InlineKeyboardButton('Поиск фильмов по жанру', callback_data='search_by_genre')],
+                [InlineKeyboardButton('Назад', callback_data='start')]]
+    markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Вы можете найти фильмы, по заданным вами параметрам.', reply_markup=markup)
