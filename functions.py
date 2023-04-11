939b68699dabdf2db9d67fc4a284d2efc0077af1
maver-ya <maver2005@ya.ru>
2023-04-11 15:14:05 +1000

    1st commit 11/04/2023

--- functions.py
+++ functions.py
@@ -1,6 +1,24 @@
-from telegram.ext import Application, MessageHandler, filters
-from config import BOT_TOKEN
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import aiohttp
 
 
 async def start(update, context):
-    pass
+    keyboard = [[InlineKeyboardButton("Поиск фильма", callback_data='search'),
+                 InlineKeyboardButton("Оценки фильмов", callback_data='assessments')],
+                [InlineKeyboardButton("Мои фильмы", callback_data='my_movies'),
+                 InlineKeyboardButton("Подборки", callback_data='mixes')],
+                [InlineKeyboardButton("Рандом", callback_data='random')]
+                ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("HELLO", reply_markup=reply_markup)
+
+
+async def button(update, context):
+    query = update.callback_query
+    await query.answer()
+    print(query.data)
+    await query.edit_message_text(f"Вы нажали {query.data}")
+
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
 from functions import *
-from telegram.ext import Application, MessageHandler, filters
-from telegram import InlineKeyboardMarkup
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from config import BOT_TOKEN
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -13,6 +14,9 @@ logger = logging.getLogger(__name__)
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
     application.run_polling()
 
 
