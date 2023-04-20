a3307928d154395722cb089593aada01d07c347e
REDKARASIK <redkarasik@yandex.ru>
2023-04-20 16:52:34 +1000

    pip8 in db_functions.py

--- db_functions.py
+++ db_functions.py
@@ -54,6 +54,7 @@ def get_all_later(id):
         result = cur.execute(f'select want_films from user where chat_id = {id}').fetchall()
         return result
 
+
 def get_all_from_films():
     with sqlite3.connect('data/users_db.sqlite3') as db_file:
         cur = db_file.cursor()
